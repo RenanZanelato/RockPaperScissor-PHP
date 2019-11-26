c3ad8d0c2ec8d5607a8d7d3bf0dee84a49cd493d
Renan Zanelato <renanzanelato96@gmail.com>
2019-11-26 16:40:02 -0300

    First Commit, Criação da Regra, porem ainda não esta funcionando os ROUNDS

--- Exceptions/AppLogicException.php
+++ Exceptions/AppLogicException.php
@@ -1,11 +1,17 @@
 <?php
 namespace RockPaperScissor\Exceptions;
 
+
+
+/**
+ * Classe NoSuchStrategyError
+ * 
+ * @name NoSuchStrategyError
+ * @author renan.zanelato <renan.zanelato@gazin.com.br>
+ * @date   26/11/2019
+ * @access public
+ */
 class AppLogicException extends \Exception
 {
-
-    public function __construct(string $message = "", int $code = 0, \Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+    
 }
